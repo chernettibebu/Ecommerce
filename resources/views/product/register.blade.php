04b7098fd5246b06255e09602371c5e044a4f250
chernet <chernettibebu1990@gmail.com>
2022-04-20 20:09:48 +0100

    first commit

---
+++ resources/views/product/register.blade.php
@@ -0,0 +1,40 @@
+<Html>
+    <header>
+        <title> ecommerce</title>
+</header>
+<body >
+    <center>
+        <font color="blue">
+        
+        <h1 > REGISTRATION PAGE</h1>
+</font>
+        <form>
+            <table>
+                <tr>
+                    <td> Name:<td>
+                        <td><input type="name" name="name" id="name"> </td>
+</tr>
+<tr>
+                    <td> Unit:<td>
+                        <td><input type="unit" name="unit" id="unit"> </td>
+</tr>
+<tr>
+                    <td> Price:<td>
+                        <td><input type="price" name="price" id="price"> </td>
+</tr>
+<tr>
+                    <td> Quantity:<td>
+                        <td><input type="quantity" name="quantity" id="quantity"> </td>
+</tr>
+<tr>
+</table>
+
+</form>
+</center>
+<center>
+<td><input type="button" name="button" value="Register"></td>
+</center>
+</body>
+</html>
+    
+
